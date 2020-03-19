340de9825cbed4c1cd40c329e85b6148488c0c66
md2eoseo <md2eoseo@gmail.com>
2020-03-19 13:34:44 +0100

    fetching svg file with parcel

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 "use strict";
+require("@babel/polyfill");
+
 window.addEventListener("DOMContentLoaded", start);
 
 const HTML = {};
 
-function start() {
-  console.log("script.js loaded");
+async function start() {
+  const response = await fetch("svg/computer_off.svg");
+  const svgData = await response.text();
+  document.querySelector("section").innerHTML = svgData;
+  startManipulatingTheSvg();
+}
+
+function startManipulatingTheSvg() {
+  console.log("startManipulatingTheSvg()");
+  // HTML.colors = document.querySelectorAll(".st2, .st3, .st4");
+  // HTML.shapes = document.querySelectorAll(".st5");
+
+  // HTML.colors.forEach(ele =>
+  //   ele.addEventListener("click", function(e) {
+  //     console.log("colors!!");
+  //     color = ele.getAttribute("fill");
+  //   })
+  // );
+  // HTML.shapes.forEach(ele =>
+  //   ele.addEventListener("click", function(e) {
+  //     console.log("shapes!!");
+  //     ele.style.fill = color;
+  //   })
+  // );
 }
