2a8762ae92060fd0dc8c1e7b20a3ee53c1f11d31
md2eoseo <md2eoseo@gmail.com>
2020-03-23 19:29:14 +0100

    revised file name

---
+++ about.js
@@ -0,0 +1,12 @@
+"use strict";
+import { gsap } from "gsap";
+require("@babel/polyfill");
+
+window.addEventListener("DOMContentLoaded", start);
+
+const HTML = {};
+const SOUND = {};
+
+async function start() {
+  console.log("start()");
+}
