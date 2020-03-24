84fa4574940f8cd012f7cec3a05859e6b67179ff
md2eoseo <md2eoseo@gmail.com>
2020-03-24 19:23:29 +0100

    hide infobox before clicking bullets

--- evolution.js
+++ evolution.js
@@ -39,6 +39,10 @@ async function loadJSON() {
 function clickable() {
   console.log("clickable()");
 
+  // hide infobox at first time
+  HTML.infobox = document.querySelector("#infobox");
+  HTML.infobox.classList.add("hide");
+
   // select bullets and add click event
   document.querySelectorAll("#bullets circle").forEach((ele, i) =>
     ele.addEventListener("click", e => {
@@ -46,6 +50,9 @@ function clickable() {
       document.querySelector("#title").innerHTML = info[i].title;
       document.querySelector("#description").innerHTML = info[i].description;
       document.querySelector("#image").href.baseVal = `img/${info[i].image}`;
+
+      // remove .hide from #infobox
+      HTML.infobox.style = "";
 
       // move infoline
       HTML.infoline = document.querySelector("#infoline");
@@ -56,14 +63,6 @@ function clickable() {
       );
       HTML.infoline.setAttribute("x2", HTML.infoline.x1.baseVal.value);
       HTML.infoline.setAttribute("y2", HTML.infoline.y1.baseVal.value - 40);
-
-      // move infobox
-      // https://stackoverflow.com/questions/479591/svg-positioning
-      // HTML.infobox.setAttribute(
-      //   "transform",
-      //   `translate(${HTML.infoline.x2.baseVal.value - 25},${HTML.infoline.y2
-      //     .baseVal.value + 327})`
-      // );
     })
   );
 }
