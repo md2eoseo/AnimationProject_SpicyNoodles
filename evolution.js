3bbce0e419b0db8546c1cdb37d2f8145ed68c5ec
md2eoseo <md2eoseo@gmail.com>
2020-03-24 19:51:21 +0100

    made clickBullet() outer function and added home, back button utility

--- evolution.js
+++ evolution.js
@@ -39,30 +39,41 @@ async function loadJSON() {
 function clickable() {
   console.log("clickable()");
 
-  // hide infobox at first time
-  HTML.infobox = document.querySelector("#infobox");
-  HTML.infobox.classList.add("hide");
+  // home, back button click event
+  document.querySelector("#home-button").addEventListener("click", goIndexHTML);
+  document.querySelector("#back-button").addEventListener("click", goIndexHTML);
 
   // select bullets and add click event
   document.querySelectorAll("#bullets circle").forEach((ele, i) =>
-    ele.addEventListener("click", e => {
-      // modifying infobox with selected info data
-      document.querySelector("#title").innerHTML = info[i].title;
-      document.querySelector("#description").innerHTML = info[i].description;
-      document.querySelector("#image").href.baseVal = `img/${info[i].image}`;
-
-      // remove .hide from #infobox
-      HTML.infobox.style = "";
-
-      // move infoline
-      HTML.infoline = document.querySelector("#infoline");
-      HTML.infoline.setAttribute("x1", Math.floor(ele.cx.baseVal.value));
-      HTML.infoline.setAttribute(
-        "y1",
-        Math.floor(ele.cy.baseVal.value - ele.r.baseVal.value)
-      );
-      HTML.infoline.setAttribute("x2", HTML.infoline.x1.baseVal.value);
-      HTML.infoline.setAttribute("y2", HTML.infoline.y1.baseVal.value - 40);
+    ele.addEventListener("click", function() {
+      clickBullet(ele, i);
     })
   );
+}
+
+function goIndexHTML() {
+  window.location.href = "/index.html";
+}
+
+function clickBullet(ele, i) {
+  console.log("clickBullet()");
+
+  // modifying infobox with selected info data
+  document.querySelector("#title").innerHTML = info[i].title;
+  document.querySelector("#description").innerHTML = info[i].description;
+  document.querySelector("#image").href.baseVal = `img/${info[i].image}`;
+
+  // remove .hide from #infobox
+  HTML.infobox = document.querySelector("#infobox");
+  HTML.infobox.classList.remove("hide");
+
+  // move infoline
+  HTML.infoline = document.querySelector("#infoline");
+  HTML.infoline.setAttribute("x1", Math.floor(ele.cx.baseVal.value));
+  HTML.infoline.setAttribute(
+    "y1",
+    Math.floor(ele.cy.baseVal.value - ele.r.baseVal.value)
+  );
+  HTML.infoline.setAttribute("x2", HTML.infoline.x1.baseVal.value);
+  HTML.infoline.setAttribute("y2", HTML.infoline.y1.baseVal.value - 40);
 }
